6d5b289840f9f1141861ced340f24eb49b340b70
Tao Chen <tao.chen@tri.global>
2023-07-01 17:41:27 -0400

    fix bug in pybind binding

--- src/allegro_hand/src/AllegroControllerBindings.cpp
+++ src/allegro_hand/src/AllegroControllerBindings.cpp
@@ -12,7 +12,7 @@ PYBIND11_MODULE(AllegroControllerPy, m) {
         .def("set_joint_torques", &AllegroController::setJointTorques)
         .def("get_time_stamp", &AllegroController::getTimeStamp)
         .def("get_joint_positions", &AllegroController::getJointPositions)
-        .def("get_joint_positions_and_velocities", &AllegroController::getJointPositionsAndVelocities);
+        .def("get_joint_positions_and_velocities", &AllegroController::getJointPositionsAndVelocities)
         .def_static("ros_init", &AllegroController::rosInit)
         .def_static("ros_spin_once", &AllegroController::rosSpinOnce);
 }
